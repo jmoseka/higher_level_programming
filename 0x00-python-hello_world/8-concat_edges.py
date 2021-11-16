ff58246129d5c24fd4573f9c70efca2a35ab3249
jmoseka <61447440+jmoseka@users.noreply.github.com>
2021-11-16 14:47:35 +0300

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "{o} {w} {p}".format(o='object-oriented programming', w='with', p='Python')
+str = str[39:67] + str[107:112] + str[0:6]
 print(str)
