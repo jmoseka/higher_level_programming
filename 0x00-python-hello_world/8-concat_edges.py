b6eafe7689e1d06936557f3c495ff6c29b071f05
jmoseka <jamylam3@gmail.com>
2021-11-16 11:33:33 +0000

    print a part of string using format

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
 str = "{o} {w} {p}".format(o='object-oriented programming', w='with', p='Python')
 print(str)
