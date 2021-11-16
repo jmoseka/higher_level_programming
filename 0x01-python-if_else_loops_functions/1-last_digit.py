9203addfcf3cdc9416c09ca3e2e25938312ee351
jmoseka <jamylam3@gmail.com>
2021-11-16 21:43:56 +0300

    analayze the last digit of random no.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,18 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     lastNumber = (number * -1) % 10
-    if lastNumber == 0:
-        print("Last digit of {:d} is {:d} and is 0".format(number, lastNumber))
-    elif lastNumber > 5:
-        print("Last digit of {:d} is {:d} and is greater than 5".format(number, lastNumber))
-    else:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, lastNumber))
-
 else:
     lastNumber = number % 10
-    if lastNumber == 0:
-        print("Last digit of {:d} is {:d} and is 0".format(number, lastNumber))
-    elif lastNumber > 5:
-        print("Last digit of {:d} is {:d} and is greater than 5".format(number, lastNumber))
-    else:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, lastNumber))
+
+if lastNumber == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, lastNumber))
+elif lastNumber > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, lastNumber))
+else:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, lastNumber))
