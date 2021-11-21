4311d05f733c4f5dd707f4ba5dba7ed9de40a690
jmoseka <61447440+jmoseka@users.noreply.github.com>
2021-11-21 13:07:23 +0300

    Checks for lowercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) > 96 and ord(c) < 123:
-        return True
-    else:
-        return False
+    for i in range(97, 123):
+         if ord(c) == i:
+             return True
