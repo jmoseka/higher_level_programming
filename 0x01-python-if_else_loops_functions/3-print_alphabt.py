4fee20bde1918d9e30303f38152408fb2302de01
jmoseka <61447440+jmoseka@users.noreply.github.com>
2021-11-21 09:48:54 +0300

    print letters and skips q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{}".format(chr(i)), end="")
+    if(chr(i) == 'e' or chr(i) == 'q'):
+        continue
+    print("{}".format(chr(i)), end="")
