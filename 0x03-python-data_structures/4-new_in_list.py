b542a9b4954f6dcb124e309a38f1ee7e49bd18f9
jmoseka <jamylam3@gmail.com>
2021-11-23 11:04:59 +0000

    function that changes element at a specific index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     if idx < 0 or idx > len(my_list):
             return new_list
-
+    else:
     new_list[idx] = element
     return new_list
