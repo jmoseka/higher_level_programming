4701b69695f8d08ad2ac6d3d2c671034c26dbf31
jmoseka <jamylam3@gmail.com>
2021-12-07 13:33:40 +0000

    a class Square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This function define a square by private instance attribute."""
+
+
+class Square:
+    """The __init__ method initialize the values passed to the object.
+    Attributes:
+        size: its a private attribute.
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size * self.__size
